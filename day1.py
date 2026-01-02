4cfd58b169218ad85d2381490dd852a2831bd797
Preeti Singh <129533986+preetisingh172003@users.noreply.github.com>
2026-01-02 09:51:21 +0530

    Add Day1 Python Practice

---
+++ day1.py
@@ -0,0 +1,16 @@
+import random
+
+random_integer = random.randint(0,2)
+print(random_integer)
+
+# for floating number if the value is fixed
+random_float = random.random()
+print(random_float)
+
+# for floating number if the you need random floating value
+random_uniform = random.uniform(1,9)
+print(random_uniform)
+
+
+
+
